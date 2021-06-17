4982376484249e2cbb75e7e9085a7ec1fc0100e9
Juanchis <juanchismo10@gmail.com>
2021-06-17 13:20:33 -0300

    React router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
-import MoviesGrid from './MoviesGrid.jsx';
-import styles from './App.module.css';
+import MoviesGrid from "./components/MoviesGrid.jsx";
+import styles from "./App.module.css";
+import { MovieDetails } from "./pages/MovieDetails.jsx";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { LandingPage } from "./pages/LandingPage.jsx";
 
 function App() {
-    return (
+  return (
+    <Router>
       <div>
         <header>
-          <h1 className={styles.title}>Movies</h1>
+          <Link to="/">
+            <h1 className={styles.title}>Movies</h1>
+          </Link>
+          <Link to="/">Movies</Link>
+          <Link to="/movie">Movie</Link>
         </header>
         <main>
-          <MoviesGrid />
+          <Switch>
+            <Route exact path="/movie">
+              <MovieDetails />
+            </Route>
+            <Route path="/">
+              <LandingPage />
+              Home
+            </Route>
+          </Switch>
         </main>
       </div>
-    );
+    </Router>
+  );
 }
 export default App;
